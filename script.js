d790ec2914fd227aef460cfe536ff173f42a98ca
Bintou-Traore <bintou.traore24@outlook.fr>
2023-06-19 23:48:34 +0200

    js and css edit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const month_names = [
   "December",
 ];
 
-let month_picker = document.querySelector("#month_picker");
+let month_picker = document.querySelector("#month-picker");
 const dayTextFormate = document.querySelector(".day-text-formate");
 const timeFormate = document.querySelector(".time-formate");
 const dateFormate = document.querySelector(".date-formate");
@@ -58,6 +58,7 @@ const generateCalendar = (month, year) => {
     30,
     31,
   ];
+
   let currentDate = new Date();
   month_picker.innerHTML = month_names[month];
   calendar_header_year.innerHTML = year;
@@ -87,7 +88,7 @@ month_names.forEach((e, index) => {
   month.onclick = () => {
     currentMonth.value = index;
     generateCalendar(currentMonth.value, currentYear.value);
-    month_list.classList.replace("show, hide");
+    month_list.classList.replace("show", "hide");
     dayTextFormate.classList.remove("hideTime");
     dayTextFormate.classList.add("showtime");
     timeFormate.classList.remove("hideTime");
@@ -125,7 +126,7 @@ const showCurrentDateOption = {
   weekday: "long",
 };
 
-const currentDateFormate = new Int1.DateTimeFormat(
+const currentDateFormate = new Intl.DateTimeFormat(
   "en-US",
   showCurrentDateOption
 ).format(currshowDate);
@@ -137,13 +138,13 @@ setInterval(() => {
     minute: "numeric",
     second: "numeric",
   };
-  const formateTimer = new Int1.DateTimeFormat("en-us", option).format(timer);
+  const formateTimer = new Intl.DateTimeFormat("en-us", option).format(timer);
   let time = `${`${timer.getHours()}`.padStart(
     2,
     "0"
   )}:${`${timer.getMinutes()}`.padStart(
     2,
     "0"
-  )}:${`${timer.getSeconds()}`.padStart(2, "0")}`;
+  )}: ${`${timer.getSeconds()}`.padStart(2, "0")}`;
   todayShowTime.textContent = formateTimer;
 }, 1000);
